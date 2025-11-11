c0087be8fa4f5b891e11fe90b3208da7a798eb88
JonasmedJ <jonas@oxboll.dk>
2025-11-11 12:50:47 +0100

    Update

---
+++ Powershell/Aduser.ps1
@@ -0,0 +1,16 @@
+# $domain = sko.butik
+
+$winsrv01 = "10.101.59.66"
+$winsrv02 = "10.101.7.6"
+$PW = Get-Credential
+$When = ((Get-Date).AddDays(-30)).Date
+
+Invoke-Command -ComputerName $winsrv01 -Credential $PW -ScriptBlock {
+    Get-ADUser -Filter {WhenCreated -ge $Using:When} -Searchbase "CN=Users,dc=SKO,dc=BUTIK" -Properties whenCreated
+}
+
+#Get-ADUser -Filter * -Searchbase "CN=Users,dc=SKO,dc=BUTIK" | 
+
+#Invoke-Command -ComputerName $winsrv01 -Credential $PW -ScriptBlock {
+#     Get-Process -IncludeUserName | Select-Object -Unique -Property UserName
+#}
---
+++ Powershell/Psdrive.ps1
@@ -0,0 +1,13 @@
+$winsrv01 = "10.101.59.66"
+$winsrv02 = "10.101.7.6"
+$PW = Get-Credential
+
+$FileSpecs="{$_.Free/1GB}, {$_.Used/1GB}, Root, PSComputerName, Name, Description"
+
+$StorageFile="C:\Windows\checkdrive_remote_$((Get-Date).ToString("dd_MM_yyyy")).csv"
+
+Invoke-Command -ComputerName $winsrv01 -Credential $PW -ScriptBlock {
+    Get-PSDrive | 
+    Select-Object $FileSpecs |
+    Export-Csv -path $StorageFile -Encoding utf8 -NoTypeInformation
+}
