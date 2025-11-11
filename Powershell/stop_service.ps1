b383b7c3424ab9de12616988ad8a3b6aefb07169
JonasmedJ <jonas@oxboll.dk>
2025-11-11 12:10:07 +0100

    Update

--- Powershell/stop_service.ps1
+++ Powershell/stop_service.ps1
@@ -1,9 +1,14 @@
+$winsrv01 = "10.101.59.66"
 $winsrv02 = "10.101.7.6"
 $PW = Get-Credential
 
-Invoke-Command -Credential $PW -ComputerName $winsrv02 -ScriptBlock {
-    (Get-WmiObject -Class Win32_Service -Filter "name='bthserv").StartService()
+#Invoke-Command -Credential $PW -ComputerName $winsrv02 -ScriptBlock {
+#    (Get-WmiObject -Class Win32_Service -Filter "name='bthserv'").StopService()
+#}
+
+$parameters = @{
+  ComputerName	    = $winsrv01, $winsrv02
+  Credential	    = $PW
+  ScriptBlock	    = (Get-WmiObject -Class Win32_Service ).StopService()
 }
-
-
-
+Invoke-Command @parameters
