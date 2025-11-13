9bc2354a8ac41d059ad424b7b272a38883d56a7a
JonasmedJ <jonas@oxboll.dk>
2025-11-13 09:58:30 +0100

    Update

--- Powershell/Case4.ps1
+++ Powershell/Case4.ps1
@@ -1,11 +1,29 @@
 $winsrv01 = "10.101.59.66"
 #$winsrv02 = "10.101.7.6"
 $PW = Get-Credential
-$StorageFile = "C:\Windows\checkdrive_remote_$((Get-Date).ToString("dd_MM_yyyy")).csv"
-$Sortitems = 
+$StorageFile = "C:\Users\jox\Documents\checkdrive1_remote_$((Get-Date).ToString("dd_MM_yyyy")).csv"
 
 Invoke-Command -ComputerName $winsrv01 -Credential $PW -ScriptBlock {
-    Get-CimInstance -ClassName Win32_ComputerSystem |
-    Where-Object $Sortitems
+    Get-CimInstance -ClassName Win32_ComputerSystem
 } |
+
+ForEach-Object {
+    $obj = $_
+    $properties = [ordered]@{}
+
+    $obj.PSObject.properties |
+
+    Where-Object {
+        $null -ne $_.Value -and $_.Value -ne ""
+    } |
+
+    Sort-Object -Property Name |
+
+    ForEach-Object {
+        $properties[$_.Name] = $_.Value
+    }
+    
+    [PSCustomObject]$properties
+}|
+
 Export-Csv -Path $StorageFile -Encoding utf8 -NoTypeInformation
